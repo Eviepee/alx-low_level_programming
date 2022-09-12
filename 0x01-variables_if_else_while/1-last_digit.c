04df4a21dd649238aadeb40ca36a4c6fb353ea9c
Evelyn Peterson <epeterson094@outlook.com>
2022-09-12 07:47:39 -0700

    Negative and Positive numbers

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,6 +11,7 @@
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
@@ -26,5 +27,7 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, n % 10);
 	}
+
 	return (0);
+
 }
