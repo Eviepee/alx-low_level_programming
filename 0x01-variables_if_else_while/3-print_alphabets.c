3cbf70316818772641a4e45f57393240f7a4c707
Evelyn Peterson <epeterson094@outlook.com>
2022-09-12 19:40:18 -0700

    print a lowercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+/**
+ * main - Entry point 
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+
+	char alphabet;
+
+	char alphabet2;
+
+	alphabet = 'a';
+	alphabet2 = 'A';
+	while
+	(alphabet <= 'z') {
+		putchar(alphabet);
+		alphabet++;
+	}
+	while
+	(alphabet <= 'Z') {
+		putchar(alphabet2);
+		alphabet2++;
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char d;
+
+	d = 'a';
+	while
+		(d <= 'z') {
+			if ((d != 'q' && d != 'e') && d <= 'z')
+			       putchar(d);
+			d++;
+		}
+	putchar('\n');
+	return (0);
+}
