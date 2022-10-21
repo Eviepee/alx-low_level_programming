be8159f4321e37b5adf2fa7180f004cf1ef4574c
Evelyn Peterson <epeterson094@outlook.com>
2022-10-21 13:06:07 -0700

    Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include "lists.h"
-/*
+
+/**
 *print_list - prints all the elements of a linked list
 *@h: pointer to the list
+*
 *Return: number of nodes printed
-**/
+*/
+
 size_t print_list(const list_t *h)
 {
 	size_t n = 0;
